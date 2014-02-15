c54df4b054a4e0b708c775077086f96fb13a0abd
Alexander van der Zalm <alexandervdzalm@gmail.com>
2014-02-15 15:36:16 +0100

    dll

---
+++ Assets/Scripts/Managers/Controls/ControlScheme.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ControlScheme : MonoBehaviour 
+{
+    public int controllerID;
+    public int playerID;
+    public List<Control> Controls;
+}
