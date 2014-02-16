f844bd89d2f2dc9f1b1d303f1e689bf5b64ac87c
Alexander van der Zalm <alexandervdzalm@gmail.com>
2014-02-16 11:36:30 +0100

    Cleaned controller

--- Assets/Scripts/GamePlay/PlayerController.cs
+++ Assets/Scripts/GamePlay/PlayerController.cs
@@ -2,8 +2,10 @@
 using System.Collections;
 
 [RequireComponent(typeof(ControlScheme))]
-public class PlayerController : MonoBehaviour 
+public class PlayerController : MonoBehaviour
 {
+    #region Fields
+
     public ControlScheme ControlScheme;
 
     public enum PlayerActions
@@ -18,20 +20,18 @@ public class PlayerController : MonoBehaviour
     public float JumpMaxForce = 500;
     public float JumpMaxHoldTime = 1.0f;
 
-    //public float RunAccelPSec = 5;
     public float RunMaxVelocity = 10;
     public float RunAccelTime = 0.5f;
     public float RunDeAccelTime = 0.25f;
 
-
     private bool facingRight = true;
     private bool grounded;
 
-    private float lastTime = 0;
+    #endregion
 
-    private bool canIHasAccel = false;
+    #region Start
 
-	// Use this for initialization
+    // Use this for initialization
 	void Start () 
     {
         ControlScheme = new ControlScheme();
@@ -52,121 +52,79 @@ public class PlayerController : MonoBehaviour
         ControlScheme.Actions[(int)PlayerActions.Jump].Keys.Add(ControlKey.XboxButton(XboxCtrlrInput.XboxButton.A));
 
 	}
-	
-	// Update is called once per frame
+
+    #endregion
+
+    #region FixedUpdate
+    
 	void FixedUpdate () 
     {
-        // Move hor
+        // hor: Input & Dir is a mini cached var
         float hor = ControlScheme.Horizontal.Value();
-
         float dir = rigidbody2D.velocity.x;
 
+        // do some precalculaltions 
         float accel = Time.fixedDeltaTime * RunMaxVelocity;
-        
         float dirNormalized = dir/Mathf.Abs(dir);
 
-        //float dt = Time.fixedDeltaTime;
-        //float mv = RunMaxVelocity;
-        //float t = RunAccelTime;
-
-        //Debug.Log("Velocity: " + rigidbody2D.velocity);// + " h: " + hor + " d: " + dirNormalized);// +" h: " + horNormalized);
-        //Debug.Log("dt: " + dt + " t: " + t + " mv: " + mv + " a: " + dt * mv / t + " dt?:" + (ct - lastTime));
-
-
+        #region Movement
         // Passive
         if (hor == 0 && dir == 0)
         {
            //// Debug.Log("Passive  ");
-           // if (!canIHasAccel)
-           // {
-           //     canIHasAccel = true;
-           // }
-
         }//DeAccel
         else if (hor == 0 && dir != 0 || (Mathf.Abs(dir) > 0 && dirNormalized != hor / Mathf.Abs(hor)))
         {
             // Possible to do fraction deaccel if wanted
             accel *= -dirNormalized / RunDeAccelTime;
-            //Debug.Log("DeAccel a: " + accel);
 
-            // If it ends up going in the other direction after accel, clamp it to 0
+            // CLAMP: If it ends up going in the other direction after accel, clamp it to 0
             float newXVelocity = dir + accel;
             if (newXVelocity / Mathf.Abs(newXVelocity) != dirNormalized)
-            {
                 rigidbody2D.velocity = new Vector2(0, rigidbody2D.velocity.y);
-                //Debug.Log("CLAMP 0000");
-            }
             else
                 rigidbody2D.velocity += new Vector2(accel, 0);
         }
         else
         {//Accel
             accel *= hor / RunAccelTime;
+            //CLAMP
             if (Mathf.Abs(dir + accel) > RunMaxVelocity)
-            {
                 rigidbody2D.velocity = new Vector2(RunMaxVelocity * dirNormalized, rigidbody2D.velocity.y);
-
-                //Debug
-
-                if (!canIHasAccel)
-                {
-
-                    float ct = Time.timeSinceLevelLoad;
-                    Debug.Log("CLAMP MAXXX" + (ct - lastTime));
-                    lastTime = ct;
-                    canIHasAccel = true;
-
-                }
-            }
             else
-            {
                 rigidbody2D.velocity += new Vector2(accel, 0);
-                //Debug.Log("Accel: " + accel);
-                //Debug
-                if (canIHasAccel)
-                {
-                    lastTime = Time.timeSinceLevelLoad;
-                    canIHasAccel = false;
-                }
-            }
-
-            //Debug.Log("Accel a: " + accel);
         }
-        
-        
-            
-        
-        
-        //accel /= RunDeAccelTime;
-        //if (rigidbody2D.velocity.x > 0)
-        //    accel *= -1;
-        
+        #endregion
 
-        //Debug.Log("fd: " + Time.fixedDeltaTime + " accel: " + accel);
-
-        
-
+        #region Flip sprite direction
+        // Face the right direction
         if (hor > 0 && !facingRight)
             Flip();
         else if(hor < 0 && facingRight)
             Flip();
+        #endregion
 
         // Jump
         if (ControlScheme.Actions[(int)PlayerActions.Jump].IsPressed())
         {
             //Start jump charge coroutine
         }
-
-        //Debug.Log("Velocity2: " + rigidbody2D.velocity);
 	}
 
+    #endregion
+
+    #region Flip
     private void Flip()
     {
         facingRight = !facingRight;
         Vector3 localScale = transform.localScale;
         localScale.x *= -1;
         transform.localScale = localScale;
-
     }
+    #endregion
 
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log("Trigger: "+ other.name);
+    }
 }
