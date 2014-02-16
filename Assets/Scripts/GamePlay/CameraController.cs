d7a234434c1835ad6094a7e7506071fe4fe4d70c
Alexander van der Zalm <alexandervdzalm@gmail.com>
2014-02-16 17:28:56 +0100

    Simple Camera & Cleaning Folders

--- Assets/Scripts/GamePlay/CameraController.cs
+++ Assets/Scripts/GamePlay/CameraController.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour 
+{
+
+    public Transform Target;
+    public float followSpeed = 1f;
 
 	// Use this for initialization
 	void Start () {
@@ -9,7 +13,11 @@ public class CameraController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
+	    Vector3 target = Target.position;
+        target.z = transform.position.z;
+
+        transform.position = Vector3.Lerp(transform.position, target, followSpeed * Time.deltaTime);
 	}
 }
