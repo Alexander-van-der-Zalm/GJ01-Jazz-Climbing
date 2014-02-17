c7d7014b18a3b421d3505ec480e302a5d6cb9359
Alexander van der Zalm <alexandervdzalm@gmail.com>
2014-02-17 14:36:14 +0100

    Fixed Some Bugs

--- Assets/Scripts/GamePlay/PlayerController.cs
+++ Assets/Scripts/GamePlay/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(ControlScheme))]
 public class PlayerController : MonoBehaviour
@@ -52,7 +53,9 @@ public class PlayerController : MonoBehaviour
     //public float 
     public float FallGravity = 50;
     public float SlideGravity = 5;
-    public float SlideInitialVelocity = 5;
+    public float SlideInitialVelocity = 2;
+    [Range(0,1)]
+    public float SlideJumpFraction = 0.5f;
 
     private ControlScheme ControlScheme;
     public PlayerState playerState;
@@ -60,14 +63,15 @@ public class PlayerController : MonoBehaviour
     private bool facingRight = true;
     
     
-    public bool grounded;
+    public bool Grounded;
 
     // Input mini-cache
     private float HorizontalInput = 0;
     private float VerticalInput = 0;
     private bool InputJump = false;
 
-    private int lastGrabbedID = -1;
+    private List<int> lastSlided = new List<int>();
+    private int lastGrabbedID = 0;
 
     private Transform WallDetector, Grab;
     private Animator animator;
@@ -94,8 +98,8 @@ public class PlayerController : MonoBehaviour
         ControlScheme.Vertical = new Axis(ControlScheme,"Vertical");
         ControlScheme.Vertical.AxisKeys.Add(AxisKey.XboxAxis(XboxCtrlrInput.XboxAxis.LeftStickY));
         ControlScheme.Vertical.AxisKeys.Add(AxisKey.XboxDpad(AxisKey.HorVert.Vertical));
-        ControlScheme.Vertical.AxisKeys.Add(AxisKey.PC(KeyCode.W, KeyCode.S));
-        ControlScheme.Horizontal.AxisKeys.Add(AxisKey.PC(KeyCode.UpArrow, KeyCode.DownArrow));
+        ControlScheme.Vertical.AxisKeys.Add(AxisKey.PC(KeyCode.S, KeyCode.W));
+        ControlScheme.Vertical.AxisKeys.Add(AxisKey.PC(KeyCode.DownArrow, KeyCode.UpArrow));
 
         ControlScheme.Actions.Insert((int)PlayerActions.Jump, new Action(ControlScheme,PlayerActions.Jump.ToString()));
         ControlScheme.Actions[(int)PlayerActions.Jump].Keys.Add(ControlKey.PCKey(KeyCode.Space));
@@ -143,7 +147,7 @@ public class PlayerController : MonoBehaviour
             return;
         }
 
-        if (!grounded&&playerState == PlayerState.WallSliding)
+        if (!Grounded&&playerState == PlayerState.WallSliding)
         {
             HandleWallSliding();
             ResetAtEndOfUpdate();
@@ -156,12 +160,15 @@ public class PlayerController : MonoBehaviour
         // Passive
         if (hor == 0 && dir == 0)
         {
-            if(grounded)
+            if (Grounded)
+            {
                 SetState(PlayerState.Idle);
+            }
+
         }//DeAccel
         else if (hor == 0 && dir != 0 || (Mathf.Abs(dir) > 0 && dirNormalized != hor / Mathf.Abs(hor)))
         {
-            if(grounded)
+            if(Grounded)
                 SetState(PlayerState.Running);
             
             // Possible to do fraction deaccel if wanted
@@ -176,7 +183,7 @@ public class PlayerController : MonoBehaviour
         }
         else
         {//Accel
-            if (grounded)
+            if (Grounded)
                 SetState(PlayerState.RunningStopping);
 
             accel *= hor / RunSettings.RunAccelTime;
@@ -197,7 +204,7 @@ public class PlayerController : MonoBehaviour
         #endregion
 
         // Jump
-        if (grounded && InputJump)
+        if (Grounded && InputJump)
         {
             Jump();
         }
@@ -206,8 +213,6 @@ public class PlayerController : MonoBehaviour
 
         ResetAtEndOfUpdate();
 	}
-
-    
 
     private void SetInputValues()
     {
@@ -224,7 +229,7 @@ public class PlayerController : MonoBehaviour
     {
         //Reset grounded
         if(playerState == PlayerState.Airborne || playerState == PlayerState.Falling || playerState == PlayerState.Grabbing)
-            grounded = false;
+            Grounded = false;
 
         InputJump = false;
     }
@@ -240,7 +245,6 @@ public class PlayerController : MonoBehaviour
 
         if (InputJump)
         {
-            
             if (hor != 0)
             {
                 if (hor < 0 && facingRight || hor > 0 && !facingRight)
@@ -256,14 +260,63 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    #region GrabTriggers
+
+    private void OnWallTriggerEnter(Collider2D other)
+    {
+        int id = other.GetInstanceID();
+
+        if (other.tag == "GrabMe" && id != lastGrabbedID)
+        {
+            // Grab the object
+            int dir = 1;
+            if (!facingRight)
+                dir = -1;
+
+            Vector3 offset = GrabOffset;
+            offset.x *= dir;
+
+            transform.position = other.transform.position - offset;
+
+            SetState(PlayerState.Grabbing);
+            lastGrabbedID = id;
+        }
+    }
+
+    private void LeaveWallTrigger(Collider2D other)
+    {
+        int id = other.GetInstanceID();
+
+        if (other.tag == "GrabMe" && lastGrabbedID == id)
+        {
+            lastGrabbedID = 0;
+        }
+    }
+
+    #endregion
+
     #endregion
 
     #region WallJump & Slide
-    
+
     private void HandleWallSliding()
     {
-        if(rigidbody2D.velocity.y < 0)
-            SetGravity(SlideGravity);
+        if (InputJump)
+        {
+            Debug.Log("WallJump");
+            float sideVelFraction = SlideJumpFraction;
+
+            if (HorizontalInput < 0 && facingRight || HorizontalInput > 0 && !facingRight)
+                sideVelFraction = 1;
+
+            rigidbody2D.velocity = new Vector2(sideVelFraction * RunSettings.RunMaxVelocity, 0);
+
+            if (VerticalInput < 0)
+                Fall(true);
+            else
+                Jump();
+        }
+            
     }
 
     #endregion
@@ -324,6 +377,7 @@ public class PlayerController : MonoBehaviour
         float v = Mathf.Sqrt(2*g*h);
 
         //Debug.Log("g: " + g + " v " + v + " gnow " + Physics2D.gravity.y);
+        //Debug.Log(rigidbody2D.velocity.x);
 
         rigidbody2D.gravityScale = g / Mathf.Abs(Physics2D.gravity.y);
         rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, v);
@@ -336,10 +390,12 @@ public class PlayerController : MonoBehaviour
         while (flyTime < t && ControlScheme.Actions[(int)PlayerActions.Jump].IsDown())
         {
             flyTime = Time.timeSinceLevelLoad - timeStart;
+            //Debug.Log(rigidbody2D.velocity.x);
             yield return null;
         }
-
-        rigidbody2D.gravityScale = 10; 
+        Debug.Log("JUMP");
+        if(playerState == PlayerState.Airborne)
+            rigidbody2D.gravityScale = 10; 
     }
 
     #endregion
@@ -360,46 +416,13 @@ public class PlayerController : MonoBehaviour
     {
         
         // Fall gravity
-        if (rigidbody2D.velocity.y < 0 || falldown)
+        if (!Grounded && (rigidbody2D.velocity.y < 0 || falldown))
         {
+            //Debug.Log("FALLING " + falldown + " " + playerState);
             SetState(PlayerState.Falling);
             SetGravity(FallGravity);
-            //rigidbody2D.gravityScale = FallGravity / Mathf.Abs(Physics2D.gravity.y);
-        }
-    }
-
-    #endregion
-
-    #region Grab
-
-    private void OnWallTriggerEnter(Collider2D other)
-    {
-        int id = other.GetInstanceID();
-
-        if (other.tag == "GrabMe" && id != lastGrabbedID)
-        {
-            // Grab the object
-            int dir = 1;
-            if (!facingRight)
-                dir = -1;
-
-            Vector3 offset = GrabOffset;
-            offset.x *= dir;
             
-            transform.position = other.transform.position - offset;
-
-            SetState(PlayerState.Grabbing);
-            lastGrabbedID = id;
-        }
-    }
-
-    private void LeaveWallTrigger(Collider2D other)
-    {
-        int id = other.GetInstanceID();
-
-        if (other.tag == "GrabMe" && lastGrabbedID == id)
-        {
-            lastGrabbedID = 0;
+            //rigidbody2D.gravityScale = FallGravity / Mathf.Abs(Physics2D.gravity.y);
         }
     }
 
@@ -413,7 +436,7 @@ public class PlayerController : MonoBehaviour
         animator.SetBool("WallSlide", false);
         rigidbody2D.isKinematic = false;
 
-        //Debug.Log(state);
+        //Debug.Log(state + " vel: " + rigidbody2D.velocity);
 
         switch (state)
         {
@@ -424,8 +447,11 @@ public class PlayerController : MonoBehaviour
 
                 break;
             case PlayerState.WallSliding:
+                
                 animator.SetBool("WallSlide", true);
+                SetGravity(SlideGravity);
                 rigidbody2D.velocity = new Vector2(0, -SlideInitialVelocity);
+                Debug.Log("StartSlide grav: " + rigidbody2D.gravityScale);
                 break;
             case PlayerState.Grabbing:
                 animator.SetBool("Grab", true);
@@ -455,15 +481,64 @@ public class PlayerController : MonoBehaviour
     {
         if (other.tag == "Floor")
         {
-            grounded = true;
+            Grounded = true;
+            return;
         }
 
-        if (playerState != PlayerState.WallSliding && !grounded && other.tag == "Wall" && rigidbody2D.velocity.y < 0)
+        int id = GetInstanceID();
+
+        if (other.tag == "Wall")
         {
-            SetState(PlayerState.WallSliding);
-            //Debug.Log("HUG LE WALL");
+            if (Grounded && lastSlided.Count != 0)
+            {
+                //string str = "Before Cleared GR [";
+                //foreach (int i in lastSlided)
+                //    str += i + ",";
+                //str += "] " + playerState;
+                //Debug.Log(str);
+
+                lastSlided.Clear();
+                return;
+            }
+
+            if (!lastSlided.Contains(id) && playerState == PlayerState.WallSliding)
+            {
+                lastSlided.Add(id);
+                //string str = "After Add WhileSliding [";
+                //foreach (int i in lastSlided)
+                //    str += i + ",";
+                //str += "] " + playerState;
+                //Debug.Log(str);
+                return;
+            }
+
+            if (!Grounded && playerState != PlayerState.WallSliding && rigidbody2D.velocity.y < 0 && lastSlided.Count == 0)
+            {
+                lastSlided.Add(id);
+                SetState(PlayerState.WallSliding);
+
+                Debug.Log("NewSlide count" + lastSlided.Count);
+                //Debug.Break();
+                return;
+            }
+
         }
-       
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        int id = GetInstanceID();
+
+        if (other.tag == "Wall" && lastSlided.Contains(id))
+        {
+            string str = "Before Removed Exit [";
+            foreach (int i in lastSlided)
+                str += i + ",";
+            str += "] " + playerState;
+            Debug.Log(str);
+            lastSlided.Remove(id);
+            //Debug.Break();
+        }
     }
 
     #endregion
