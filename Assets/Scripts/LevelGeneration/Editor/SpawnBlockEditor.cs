9c398bc7386588fd7c07f8525a5641dede7859b7
Alexander van der Zalm <alexandervdzalm@gmail.com>
2014-02-20 12:02:40 +0100

    Updates blocks when size changed

--- Assets/Scripts/LevelGeneration/Editor/SpawnBlockEditor.cs
+++ Assets/Scripts/LevelGeneration/Editor/SpawnBlockEditor.cs
@@ -5,18 +5,33 @@ using UnityEditor;
 [CustomEditor(typeof(SpawnBlock))]
 public class SpawnBlockEditor : Editor 
 {
-	public override void OnInspectorGUI()
+    int oldHeight;
+    int oldWidth;
+    string originalName;
+
+    public override void OnInspectorGUI()
     {
         SpawnBlock sb = target as SpawnBlock;
         
         if(GUILayout.Button("Create New Block"))
-        {
-            if(sb.SpawnedBlock != null)
-                GameObject.DestroyImmediate(sb.SpawnedBlock);
+            CreateNewBlock(sb);
 
-            sb.SpawnedBlock = LevelGenerator.CreateSimpleLevelBlock(sb.TileSet,sb.transform.position,sb.BlockHeight,sb.BlockWidth,sb.Seed);
-            sb.SpawnedBlock.transform.parent = sb.transform;
-        }
         DrawDefaultInspector();
+
+        if(sb.BlockHeight != oldHeight || sb.BlockWidth != oldWidth)
+            CreateNewBlock(sb);
+
+
+        oldHeight = sb.BlockHeight;
+        oldWidth = sb.BlockWidth;
+    }
+
+    private void CreateNewBlock(SpawnBlock sb)
+    {
+        if (sb.SpawnedBlock != null)
+            GameObject.DestroyImmediate(sb.SpawnedBlock);
+
+        sb.SpawnedBlock = LevelGenerator.CreateSimpleLevelBlock(sb.TileSet, sb.transform.position, sb.BlockHeight, sb.BlockWidth, sb.Seed);
+        sb.SpawnedBlock.transform.parent = sb.transform;
     }
 }
