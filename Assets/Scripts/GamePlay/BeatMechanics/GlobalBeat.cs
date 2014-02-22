ff3f1814248bea9f64d9ec14f3a7bcf213761065
Alexander van der Zalm <alexandervdzalm@gmail.com>
2014-02-22 13:00:03 +0100

    BG plz

---
+++ Assets/Scripts/GamePlay/BeatMechanics/GlobalBeat.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalBeat : Singleton<GlobalBeat> 
+{
+
+	// Use this for initialization
+	void Start () 
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    
+	}
+}
